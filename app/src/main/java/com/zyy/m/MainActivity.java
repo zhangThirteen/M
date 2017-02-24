75fceaab3a8764895860c629eb7e5c8b1780f339
zhangThirteen <13925130654@163.com>
2017-02-24 10:38:07 +0800

    4
    letter

--- app/src/main/java/com/zyy/m/MainActivity.java
+++ app/src/main/java/com/zyy/m/MainActivity.java
@@ -1,42 +1,63 @@
 package com.zyy.m;
 
-import android.annotation.TargetApi;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+
+    @BindView(R.id.rounded_header_image)
+    RoundedImageView roundedHeaderImage;
+    @BindView(R.id.header_title)
+    TextView headerTitle;
+    @BindView(R.id.header_search)
+    TextView headerSearch;
+    @BindView(R.id.header)
+    LinearLayout header;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         setStatusBar();
+
+
     }
 
+
     private void setStatusBar() {
-        Window window = this.getWindow();
+        if (Build.VERSION.SDK_INT >= 23) {
+            Window window = this.getWindow();
 //取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
 //需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 //设置状态栏颜色
 
-        window.setStatusBarColor(getResources().getColor(R.color.top));
+            window.setStatusBarColor(getResources().getColor(R.color.top));
 
-        ViewGroup mContentView = (ViewGroup) this.findViewById(Window.ID_ANDROID_CONTENT);
-        View mChildView = mContentView.getChildAt(0);
-        if (mChildView != null) {
-            //注意不是设置 ContentView 的 FitsSystemWindows, 而是设置 ContentView 的第一个子 View . 预留出系统 View 的空间.
-            ViewCompat.setFitsSystemWindows(mChildView, true);
+            ViewGroup mContentView = (ViewGroup) this.findViewById(Window.ID_ANDROID_CONTENT);
+            View mChildView = mContentView.getChildAt(0);
+            if (mChildView != null) {
+                //注意不是设置 ContentView 的 FitsSystemWindows, 而是设置 ContentView 的第一个子 View . 预留出系统 View 的空间.
+                ViewCompat.setFitsSystemWindows(mChildView, true);
+            }
         }
     }
 
